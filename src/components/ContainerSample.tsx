92af6309e33cfe7a83d8fd3870e57521644103e6
HOJOON07 <ghwns107@naver.com>
2023-08-20 23:57:41 +0900

    learn Next

--- src/components/ContainerSample.tsx
+++ src/components/ContainerSample.tsx
@@ -3,6 +3,10 @@ import React from "react";
 interface Name {
   hojoon: string;
   sorrytoCommit: string;
+  naver: string;
+  performance: string;
 }
+
+console.log(10 % 3);
 
 const Container = () => {};
